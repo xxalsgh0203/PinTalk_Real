05465aab9ab23dd8b9f58baaa26dd3419ac009e0
taehee <jangth0655@gmail.com>
2022-12-21 20:49:34 +0900

    feat Spring과 React 연동

---
+++ src/test/java/com/example/pintalk/PintalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pintalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PintalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
