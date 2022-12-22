852792fea0f3cc9b072a9ce3d804b9e5a5e7878b
top <dks2922@gmail.com>
2022-12-22 22:26:27 +0900

    chore : example 패키지제거로 인한 PintalkApplication 해당 클래스 패키지 이동

---
+++ src/main/java/com/pintalk/PintalkApplication.java
@@ -0,0 +1,15 @@
+package com.pintalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PintalkApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(PintalkApplication.class, args);
+	}
+
+
+}
