941754d2153cbde2e70771dc614dab065da53a8d
top <dks2922@gmail.com>
2022-12-25 22:01:03 +0900

    refactor : @select 어노테이션 -> mybatis 변경

--- src/main/java/com/pintalk/user/controller/UserController.java
+++ src/main/java/com/pintalk/user/controller/UserController.java
@@ -1,28 +1,33 @@
 package com.pintalk.user.controller;
 
-import com.pintalk.user.mapper.UserMapper;
+import com.pintalk.user.model.UserMember;
+import com.pintalk.user.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
-import java.util.HashMap;
-
-@RestController
+@Controller
 public class UserController {
 
-    private UserMapper mapper;
+    private Logger logger = LoggerFactory.getLogger(getClass().getName());
 
-    public UserController(UserMapper mapper) {
-        this.mapper = mapper;
-    }
 
-    HashMap result = new HashMap();
+    @Autowired
+    private UserService userService;
 
-    @GetMapping("/user/{id}")
-    public String getUserMember(@PathVariable("id") String id) {
-        result = mapper.getUserMember(id);
-        System.out.println("hm_result : " + result);
+//    public void setUserService(UserService userService) {this.userService = userService;}
 
-        return (String) result.get("id");
+    @GetMapping("/user")
+    public List getUserMemberList() throws Exception {
+        List<UserMember> li_result = userService.userList();
+
+        logger.debug("===================================================================");
+        logger.debug("UserController.getUserMemberList 리스트 결과값  : " + li_result);
+        logger.debug("===================================================================");
+
+        return li_result;
     }
 }
--- src/main/java/com/pintalk/user/mapper/UserMapper.java
+++ src/main/java/com/pintalk/user/mapper/UserMapper.java
@@ -2,18 +2,12 @@ package com.pintalk.user.mapper;
 
 import com.pintalk.user.model.UserMember;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
 import java.util.List;
 
+@Repository
 @Mapper
 public interface UserMapper {
-
-    @Select("SELECT * FROM USER_MEMBER WHERE 1=1 AND ID = #{id} ")
-    HashMap getUserMember(@Param("id") String id);
-
-    @Select("SELECT * FROM USER_MEMBER WHERE 1=1")
-    List<UserMember> getUserMemberList();
+    List<UserMember> getUserList();
 }
