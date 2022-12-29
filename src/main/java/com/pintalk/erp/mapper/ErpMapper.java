e1662d92fd48ead142c11eebfdbf0007b4dd6162
top <dks2922@gmail.com>
2022-12-29 22:23:36 +0900

    feat : 투자자 전체 리스트 조회 API 추가

---
+++ src/main/java/com/pintalk/erp/mapper/ErpMapper.java
@@ -0,0 +1,19 @@
+package com.pintalk.erp.mapper;
+
+import com.pintalk.erp.model.ErpMember;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface ErpMapper {
+
+    //대출자 전체 리스트
+    List<ErpMember> erpMemberList();
+//    List<UpsMember> erpOneList(int no);
+//    int erpModify(UpsMember model);
+//    int erpMemberInsert(ErpMember model);
+//    int erpDelete(UpsMember model);
+}
