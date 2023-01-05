b93b16e9ab1a21fc0a017a68b3e74c3540b00398
top <dks2922@gmail.com>
2023-01-05 23:01:46 +0900

    feat : JPA 변경 페이징처리 추가

--- src/main/java/com/pintalk/user/controller/UserController.java
+++ src/main/java/com/pintalk/user/controller/UserController.java
@@ -1,34 +1,56 @@
 package com.pintalk.user.controller;
 
-import com.pintalk.user.model.UserMember;
+import com.pintalk.user.entity.Erp_Member_Info;
 import com.pintalk.user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+//@Controller
 @RestController
 public class UserController {
 
-    private Logger logger = LoggerFactory.getLogger(getClass().getName());
+    private Logger log = LoggerFactory.getLogger(getClass().getName());
 
     @Autowired
-    private UserService userService;
+    UserService service;
 
-    @GetMapping("/api/user")
-    public List getUserMemberList() throws Exception {
-        List<UserMember> li_result = userService.userList();
-        logger.debug("===================================================================");
-        logger.debug("UserController.getUserMemberList 리스트 결과값  : " + li_result);
-        logger.debug("===================================================================");
+    //유저 리스트
+    @GetMapping("/erpMemberList")
+    public List getErpMemberList(
+            @PageableDefault(page = 0, size = 10, sort = "no", direction = Sort.Direction.DESC) Pageable pageable,
+            String searchKeyWord) throws Exception {
 
-        return li_result;
+        Page<Erp_Member_Info> list = null;
+
+        if(searchKeyWord == null) {
+            list = service.erpMemberList(pageable);
+        } else {
+            list = service.erpMemberSearchList(searchKeyWord, pageable);
+        }
+
+        int nowPage = list.getPageable().getPageNumber() +1;
+        int startPage = Math.max(nowPage - 4, 1);
+        int endPage = Math.min(nowPage + 5, list.getTotalPages());
+
+        List result_li = new ArrayList();
+        HashMap result_hs = new HashMap();
+
+        result_hs.put("nowPage",nowPage);
+        result_hs.put("startPage",startPage);
+        result_hs.put("endPage",endPage);
+        result_li.add(list.getContent());
+        result_li.add(result_hs);
+
+        return result_li;
     }
-
-
-
 }
