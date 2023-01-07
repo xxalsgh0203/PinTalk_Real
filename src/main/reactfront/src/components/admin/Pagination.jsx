aa9500ec27e63468eccf84f8958700d38c7d51a4
Minho Kim <music4537@naver.com>
2023-01-07 20:59:15 +0900

    Refactor : No more using props, now using Redux for status manage tool and its setups

--- src/main/reactfront/src/components/admin/Pagination.jsx
+++ src/main/reactfront/src/components/admin/Pagination.jsx
@@ -1,31 +1,29 @@
+function Pagination ({ total, limit, page, setPage }) {
+  const numPages = Math.ceil(total / limit);
 
-
-function Pagination({ total, limit, page, setPage }) {
-    const numPages = Math.ceil(total / limit);
-
-    return (
-        <>
-            <nav className="flex justify-center items-center gap-4 m-16">
-                <button onClick={() => setPage(page - 1)} disabled={page === 1}>
-                    &lt;
-                </button>
-                {Array(numPages)
-                    .fill()
-                    .map((_, i) => (
-                        <button
-                            key={i + 1}
-                            onClick={() => setPage(i + 1)}
-                            aria-current={page === i + 1 ? "page" : null}
-                        >
-                            {i + 1}
-                        </button>
-                    ))}
-                <button onClick={() => setPage(page + 1)} disabled={page === numPages}>
-                    &gt;
-                </button>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <nav className="flex justify-center items-center gap-4 m-16">
+        <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+          &lt;
+        </button>
+        {Array(numPages)
+          .fill()
+          .map((_, i) => (
+            <button
+              key={i + 1}
+              onClick={() => setPage(i + 1)}
+              aria-current={page === i + 1 ? 'page' : null}
+            >
+              {i + 1}
+            </button>
+          ))}
+        <button onClick={() => setPage(page + 1)} disabled={page === numPages}>
+          &gt;
+        </button>
+      </nav>
+    </>
+  );
 }
 
 export default Pagination;
--- src/main/reactfront/src/redux/store.js
+++ src/main/reactfront/src/redux/store.js
@@ -1,1 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { userReducer } from './slices/userReducer';
+import logger from 'redux-logger';
+
+const reducer = combineReducers({ userReducer: userReducer.reducer });
+
+const store = configureStore({
+  reducer,
+  middleware: [...getDefaultMiddleware(), logger],
+});
+
+export default store;
