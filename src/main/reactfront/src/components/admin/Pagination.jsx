22b0fc5c688ae5617a5405021d3654b08ae15357
taehee <jangth0655@gmail.com>
2023-01-08 22:49:37 +0900

    feat : 리덕스 페이지 reducer 생성 및 적용

--- src/main/reactfront/src/components/admin/Pagination.jsx
+++ src/main/reactfront/src/components/admin/Pagination.jsx
@@ -1,13 +1,27 @@
-function Pagination ({ total, limit, page, setPage }) {
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { userReducer } from '../../redux/slices/userReducer';
+
+function Pagination({ total, limit }) {
+  const [page, setPage] = useState(1);
+  const pageDispatch = useDispatch();
   const numPages = Math.ceil(total / limit);
+
+  const handlePrevPage = () => {
+    setPage((prev) => (prev - 1 === 0 ? 1 : prev - 1));
+    pageDispatch(userReducer.actions.handlePage(page));
+  };
+
+  const handleAfterPage = () => {
+    setPage((prev) => prev + 1);
+    pageDispatch(userReducer.actions.handlePage(page));
+  };
 
   return (
     <>
       <nav className="flex justify-center items-center gap-4 m-16">
-        <button onClick={() => setPage(page - 1)} disabled={page === 1}>
-          &lt;
-        </button>
-        {Array(numPages)
+        <button onClick={handlePrevPage}>&lt;</button>
+        {/* {Array(numPages)
           .fill()
           .map((_, i) => (
             <button
@@ -17,10 +31,8 @@ function Pagination ({ total, limit, page, setPage }) {
             >
               {i + 1}
             </button>
-          ))}
-        <button onClick={() => setPage(page + 1)} disabled={page === numPages}>
-          &gt;
-        </button>
+          ))} */}
+        <button onClick={handleAfterPage}>&gt;</button>
       </nav>
     </>
   );
