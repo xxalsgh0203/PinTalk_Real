d764d395bbcce5710ac3112e3b85f6df5c9089ce
dks2922 <@dks92728106>
2023-01-08 15:14:54 +0900

    Refactor : erpMemberList -> userMemberList 수정

--- src/main/java/com/pintalk/user/controller/UserController.java
+++ src/main/java/com/pintalk/user/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.pintalk.user.controller;
 
-import com.pintalk.user.entity.Erp_Member_Info;
+import com.pintalk.user.entity.UserMember;
 import com.pintalk.user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,29 +25,29 @@ public class UserController {
     UserService service;
 
     //유저 리스트
-    @GetMapping("/erpMemberList")
-    public List getErpMemberList(
+    @GetMapping("/userMemberList")
+    public List getUserMemberList(
             @PageableDefault(page = 0, size = 10, sort = "no", direction = Sort.Direction.DESC) Pageable pageable,
             String searchKeyWord) throws Exception {
 
-        Page<Erp_Member_Info> list = null;
+        Page<UserMember> list = null;
 
-        if(searchKeyWord == null) {
+        if (searchKeyWord == null) {
             list = service.erpMemberList(pageable);
         } else {
             list = service.erpMemberSearchList(searchKeyWord, pageable);
         }
 
-        int nowPage = list.getPageable().getPageNumber() +1;
+        int nowPage = list.getPageable().getPageNumber() + 1;
         int startPage = Math.max(nowPage - 4, 1);
         int endPage = Math.min(nowPage + 5, list.getTotalPages());
 
         List result_li = new ArrayList();
         HashMap result_hs = new HashMap();
 
-        result_hs.put("nowPage",nowPage);
-        result_hs.put("startPage",startPage);
-        result_hs.put("endPage",endPage);
+        result_hs.put("nowPage", nowPage);
+        result_hs.put("startPage", startPage);
+        result_hs.put("endPage", endPage);
         result_li.add(list.getContent());
         result_li.add(result_hs);
 
