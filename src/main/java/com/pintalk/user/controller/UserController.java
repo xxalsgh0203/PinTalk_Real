cef3801c5307da7c962b481dc3b6e4aa849fe5c5
top <dks2922@gmail.com>
2023-01-11 23:02:55 +0900

    feat : 유저상세 페이지 View API 추가

--- src/main/java/com/pintalk/user/controller/UserController.java
+++ src/main/java/com/pintalk/user/controller/UserController.java
@@ -8,8 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -18,7 +18,6 @@ import java.util.List;
 
 //@Controller
 @RestController
-@EnableJpaAuditing
 public class UserController {
 
     private Logger log = LoggerFactory.getLogger(getClass().getName());
@@ -30,15 +29,20 @@ public class UserController {
     @GetMapping("/userMemberList")
     public List getUserMemberList(
             @PageableDefault(page = 0, size = 10, sort = "no", direction = Sort.Direction.DESC) Pageable pageable,
-            String searchKeyWord) throws Exception {
+            String searchKeyWord) {
 
         Page<UserMember> list = null;
+
+
+
 
         if (searchKeyWord == null) {
             list = service.userMemberList(pageable);
         } else {
             list = service.userMemberListSearch(searchKeyWord, pageable);
         }
+
+
 
         int currPage = list.getPageable().getPageNumber();
         int totalPage = list.getTotalPages();
@@ -56,12 +60,22 @@ public class UserController {
         result_li.add(list.getContent());
         result_li.add(result_hs);
 
-        System.out.println("result_li : " + result_li);
         return result_li;
     }
 
+    //유저상세 페이지
+    @GetMapping("/userMemberDetail")
+    public String userMemberDetail(Model model, Integer id) {
+
+        model.addAttribute("UserMemberList",service.userMemberDetail(id));
+
+        return "/userMemberDetail";
+    }
+
+    //유저 신규 처리
+    @GetMapping("/userMemberInsert")
     //유저 신규 등록
-    public void getUserMemberInsert(UserMember userMember) throws Exception {
+    public void getUserMemberInsert(UserMember userMember) {
         service.userMemberInsert(userMember);
     }
 }
--- src/main/java/com/pintalk/user/service/UserService.java
+++ src/main/java/com/pintalk/user/service/UserService.java
@@ -7,29 +7,36 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@EnableJpaAuditing
 public class UserService {
 
     @Autowired
-    public UserRepository mapper;
+    public UserRepository repository;
 
     Logger log = LoggerFactory.getLogger(getClass().getName());
 
     //유저 전체 리스트
-    public Page<UserMember> userMemberList(Pageable pageable) throws Exception {
-        return mapper.findAll(pageable);
+    public Page<UserMember> userMemberList(Pageable pageable) {
+        return repository.findAll(pageable);
     }
     //유저 개인 리스트
-    public Page<UserMember> userMemberListSearch(String searchKeyword, Pageable pageable) throws Exception {
-        return mapper.findErpMemberBy(searchKeyword,pageable);
+    public Page<UserMember> userMemberListSearch(String searchKeyword, Pageable pageable) {
+        return repository.findErpMemberBy(searchKeyword,pageable);
+    }
+
+    //유저상세 페이지
+    public UserMember userMemberDetail(Integer id) {
+        return repository.findById(id).get();
     }
 
     //유저 신규 등록
-    public UserMember userMemberInsert(UserMember userMember) throws Exception {
-        return mapper.save(userMember);
+    public UserMember userMemberInsert(UserMember userMember) {
+        return repository.save(userMember);
     }
 
 
