dd6b77dd461fc08e626ee1ed9e3fe0b1175778f8
top <dks2922@gmail.com>
2023-01-15 02:53:39 +0900

    feat : 생년, 월, 일 객체 추가

--- src/main/java/com/pintalk/user/entity/UserMember.java
+++ src/main/java/com/pintalk/user/entity/UserMember.java
@@ -18,13 +18,15 @@ public class UserMember extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USERMEMBER_GENERATOR")
     @SequenceGenerator(name = "USERMEMBER_GENERATOR", sequenceName = "USERMEMBER_SEQ", initialValue = 1, allocationSize = 1)
     private Integer no;
-
     private String id;
     private String password;
     private String name;
     private String ssn;
     private String ssn1;
     private String ssn2;
+    private String year;
+    private String month;
+    private String day;
     private String phone1;
     private String phone2;
     private String phone3;
