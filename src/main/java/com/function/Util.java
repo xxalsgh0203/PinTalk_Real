dc57949cbd533248b68c27e8f92567600dcc92d8
top <dks2922@gmail.com>
2023-01-20 23:10:07 +0900

    feat : HashMap null 값 제거 메소드 추가

--- src/main/java/com/function/Util.java
+++ src/main/java/com/function/Util.java
@@ -12,10 +12,11 @@ public class Util {
 
     /**
      * Object to Map
+     *
      * @param object
      * @return
      */
-    public HashMap convertObjectToMap(Object object) {
+    public static HashMap convertObjectToMap(Object object) {
         ObjectMapper objectMapper = new ObjectMapper();
 
         HashMap<String, Object> result = (HashMap<String, Object>) objectMapper.convertValue(object, Map.class);
@@ -24,9 +25,10 @@ public class Util {
 
     /**
      * Map values null Remove
+     *
      * @param map
      */
-    public void mapValueNullRemove(HashMap map){
+    public void mapValueNullRemove(HashMap map) {
 
         map.values().removeAll(Collections.singletonList(null));
         map.values().removeAll(Collections.singletonList(""));
@@ -35,14 +37,15 @@ public class Util {
 
     /**
      * mapKeySetToList
+     *
      * @param map
      * @return
      */
-    public List mapKeySetToList(Map map){
+    public List mapKeySetToList(Map map) {
         List result = new ArrayList();
 
         Iterator<String> keys = map.keySet().iterator();
-        while (keys.hasNext()){
+        while (keys.hasNext()) {
             String key = keys.next();
             result.add(key);
         }
@@ -51,18 +54,40 @@ public class Util {
 
     /**
      * Date Format
-     * @param dateStr 변경할 날짜
+     *
+     * @param dateStr       변경할 날짜
      * @param dateStrFormat 변경할 날짜 현재 패턴
      * @param patternFormat 변경할 날짜 바꿀 패턴
      * @return
      * @throws ParseException
      */
-    public String setDateFormatStr(String dateStr,String dateStrFormat, String patternFormat) throws ParseException {
+    public String setDateFormatStr(String dateStr, String dateStrFormat, String patternFormat) throws ParseException {
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateStrFormat);
-        Date date= simpleDateFormat.parse(dateStr);
+        Date date = simpleDateFormat.parse(dateStr);
         String result = new SimpleDateFormat(patternFormat).format(date);
 
         return result;
     }
+
+    /**
+     * HashMap null 값 제거
+     * @param map
+     * @return
+     */
+    public HashMap mapKeyValueOupPut(Map map) {
+        HashMap result = new HashMap();
+
+        Iterator<Map.Entry<Object, Object>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<Object, Object> entry = it.next();
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+            if (entry.getValue() != null) {
+                result.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return result;
+    }
+
+
 }
