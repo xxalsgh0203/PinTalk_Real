2a34489133dd58804cd9b908a08a182624e9c915
taehee <jangth0655@gmail.com>
2023-01-21 19:38:59 +0900

    feat : login-회원등록신청 route 설정 및 페이지 추가

---
+++ src/main/reactfront/src/pages/login/Register.jsx
@@ -0,0 +1,12 @@
+import RegisterForm from '../../components/register/RegisterForm';
+import RegisterTitle from '../../components/register/RegisterTitle';
+
+const Register = () => {
+  return (
+    <>
+      <RegisterTitle title="회원등록 신청서" />
+      <RegisterForm />
+    </>
+  );
+};
+export default Register;
