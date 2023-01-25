c62ffd431389dc90fd79db13c5b0c23c4ea7c779
taehee <jangth0655@gmail.com>
2023-01-26 02:36:30 +0900

    design : navbar-디자인 업데이트

--- src/main/reactfront/src/components/layout/navbar/Navbar.jsx
+++ src/main/reactfront/src/components/layout/navbar/Navbar.jsx
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom';
 import PinBankLogo from '../../../asset/pinbank_icon.png';
 import cls from '../../../utils/cls';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineClose } from 'react-icons/ai';
+import useWindowSize from '../../../hooks/useWindowSize';
+import { useEffect, useState } from 'react';
 
 const navigationItem = [
   {
@@ -15,9 +19,25 @@ const navigationItem = [
     title: '관리자',
     id: 'admin',
   },
+  {
+    title: '로그인',
+    id: 'login',
+  },
 ];
 
 const Navbar = ({ title, home }) => {
+  const { standardWindowSize } = useWindowSize();
+  const [active, setActive] = useState(false);
+
+  const toggleActiveNavbar = () => {
+    setActive((prev) => !prev);
+  };
+
+  useEffect(() => {
+    !standardWindowSize && setActive(false);
+  }, [standardWindowSize]);
+
+  console.log(active);
   return (
     <div className="relative">
       <div
@@ -31,24 +51,44 @@ const Navbar = ({ title, home }) => {
           <div className="text-kukmin-dark-brown font-bold text-xl">INBANK</div>
         </Link>
         <h1>{title}</h1>
-        <ul className="flex items-center space-x-10 text-kukmin-dark-brown font-semi-bold text-lg">
-          {navigationItem.map((item) => (
-            <li key={item.id}>
-              <Link to={`/${item.id}`}>
-                <span className="block p-2 hover:bg-gray-100 rounded-lg transition-all">
-                  {item.title}
-                </span>
-              </Link>
-            </li>
-          ))}
-          <li>
-            <Link to="/login">
-              <span className="p-2">로그인</span>
-            </Link>
-          </li>
-        </ul>
+
+        {standardWindowSize ? (
+          <div onClick={toggleActiveNavbar} className="cursor-pointer">
+            {active ? <AiOutlineClose size={25} /> : <GiHamburgerMenu size={25} />}
+          </div>
+        ) : (
+          <ul className="flex items-center space-x-10 text-kukmin-dark-brown font-semi-bold text-lg">
+            {navigationItem.map((item) => (
+              <li key={item.id}>
+                <Link to={`/${item.id}`}>
+                  <span className="block p-2 hover:bg-gray-100 rounded-lg transition-all">
+                    {item.title}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+
+        <div
+          className={cls(
+            active ? 'scale-y-100' : 'scale-y-0',
+            'absolute w-full translate-y-32 left-0 origin-top-left transition-all',
+          )}
+        >
+          <ul className="bg-white">
+            {navigationItem.map((item) => (
+              <li key={item.id}>
+                <Link to={`/${item.id}`}>
+                  <span className="block p-3 pl-8 hover:bg-gray-100 transition-all">
+                    {item.title}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="absolute bg-white opacity-70 w-full h-full" />
     </div>
   );
 };
