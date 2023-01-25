d1fc3510a7612944440b487c3fe4d14bbc0ef34c
top <dks2922@gmail.com>
2023-01-25 23:37:45 +0900

    feat : 공통코드 테스트 클래스 추가

--- src/test/java/com/pintalk/user/UserTests.java
+++ src/test/java/com/pintalk/user/UserTests.java
@@ -1,5 +1,7 @@
 package com.pintalk.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.function.Util;
 import com.pintalk.user.entity.UserMember;
 import com.pintalk.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -7,7 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+
+//@EnableJpaAuditing
 @SpringBootTest
 public class UserTests {
 
@@ -21,6 +29,28 @@ public class UserTests {
 
         UserMember userMember = UserMember.builder().id("miss").ssn(ssn).ssn1(ssn.substring(0,6)).ssn2(ssn.substring(7,13)).build();
         userRepository.save(userMember);
+
+    }
+
+
+    @Test
+    public void test1(){
+        Util util = new Util();
+        HashMap map = new HashMap();
+        map.put("ssn","1234561234567");
+
+        UserMember userMember = new UserMember();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+//        UserMember userMember = util.convertObjectToMap(map);
+        System.out.println(objectMapper.convertValue(map, UserMember.class));
+    }
+    @Test
+    public void test2() throws ParseException {
+
+        Util util = new Util();
+        System.out.println();
 
     }
 }
