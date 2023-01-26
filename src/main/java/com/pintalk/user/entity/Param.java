5a97a49028e0f948b100d48bb3570f39754f260a
top <dks2922@gmail.com>
2023-01-26 23:38:40 +0900

    feat : Param Model GetMapping 추가

--- src/main/java/com/pintalk/user/entity/Param.java
+++ src/main/java/com/pintalk/user/entity/Param.java
@@ -2,13 +2,44 @@ package com.pintalk.user.entity;
 
 import com.pintalk.common.entity.BaseEntity;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @ToString
 public class Param extends BaseEntity {
+
+    public Param getParamObject(Param param){
+        return param;
+    }
+
+    @GetMapping("/userMemberListForm")
+    public Param getParam(Param param){
+
+        param.setGender(param.getGender());
+        param.setName(param.getName());
+        param.setYear(param.getYear());
+        param.setMonth(param.getMonth());
+        param.setDay(param.getDay());
+        param.setPhone1(param.getPhone1());
+        param.setPhone2(param.getPhone2());
+        param.setPhone3(param.getPhone3());
+        param.setAddress(param.getAddress());
+        param.setEmail(param.getEmail());
+        param.setSignDateStart(param.getSignDateStart());
+        param.setSignDateEnd(param.getSignDateEnd());
+        param.setModifyDateStart(param.getModifyDateStart());
+        param.setModifyDateEnd(param.getModifyDateEnd());
+        param.setStatus(param.getStatus());
+
+        return param;
+    }
+
+
     private Integer no;             //유저 고유 번호
 
     private String id;              //유저 아이디
@@ -17,6 +48,11 @@ public class Param extends BaseEntity {
     private String ssn;             //유저 주민번호
     private String ssn1;            //유저 주민번호(앞자리)
     private String ssn2;            //유저 주민번호(뒷자리)
+
+    private String year;            //유저 생년
+    private String month;           //유저 월
+    private String day;             //유저 일
+
     private String phone1;          //유저 번호 앞자리
     private String phone2;          //유저 번호 중간자리
     private String phone3;          //유저 번호 뒷자리
@@ -39,4 +75,6 @@ public class Param extends BaseEntity {
 
     private LocalDateTime reg_Date;
     private LocalDateTime update_Date;
+
+
 }
