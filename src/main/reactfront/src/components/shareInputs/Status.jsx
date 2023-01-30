b30293021f734d0025339da3c1b1c4c87c69041b
Minho Kim <music4537@naver.com>
2023-01-30 14:57:45 +0900

    feat : Added saveStatus on user info update page(admin)

--- src/main/reactfront/src/components/shareInputs/Status.jsx
+++ src/main/reactfront/src/components/shareInputs/Status.jsx
@@ -1,29 +1,56 @@
 import cls from '../../utils/cls';
 
-const Status = ({ register, watch, editPage, ustatus, label }) => {
-  return (
-    <div className="w-full">
-      <span className="block mb-2 text-sm">{label}</span>
-      <div className="flex items-center space-x-4">
-        <select
-          className="bg-transparent p-1 outline-none text-sm border-2 rounded-md text-pintalk-dark-brown w-[25%]"
-          {...register}
-        >
-          <option value={ustatus}>
-            {ustatus === 'A' ? '승인' : ustatus === 'W' ? '탈퇴' : '대기'}
-          </option>
-          <option value="A" className={ustatus === 'A' ? 'hidden' : ''}>
-            승인
-          </option>
-          <option value="W" className={ustatus === 'W' ? 'hidden' : ''}>
-            탈퇴
-          </option>
-          <option value="P" className={ustatus === 'P' ? 'hidden' : ''}>
-            대기
-          </option>
-        </select>
+const Status = ({ register, watch, ustatus, usavestatus, label, infotype }) => {
+  if (infotype === 'status') {
+    return (
+      <div className="w-full">
+        <span className="block mb-2 text-sm">{label}</span>
+        <div className="flex items-center space-x-4">
+          <select
+            className="bg-transparent p-1 outline-none text-sm border-2 rounded-md text-pintalk-dark-brown w-[25%]"
+            {...register}
+          >
+            <option value={ustatus}>
+              {ustatus === 'A' ? '승인' : ustatus === 'W' ? '탈퇴' : '대기'}
+            </option>
+            <option value="A" className={ustatus === 'A' ? 'hidden' : ''}>
+              승인
+            </option>
+            <option value="W" className={ustatus === 'W' ? 'hidden' : ''}>
+              탈퇴
+            </option>
+            <option value="P" className={ustatus === 'P' ? 'hidden' : ''}>
+              대기
+            </option>
+          </select>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else if (infotype === 'saveStatus') {
+    return (
+      <div className="w-full">
+        <span className="block mb-2 text-sm">{label}</span>
+        <div className="flex items-center space-x-4">
+          <select
+            className="bg-transparent p-1 outline-none text-sm border-2 rounded-md text-pintalk-dark-brown w-[50%]"
+            {...register}
+          >
+            <option value={usavestatus}>
+              {ustatus === 'A' ? '가입승인' : ustatus === 'W' ? '서류반려' : '대기'}
+            </option>
+            <option value="A" className={ustatus === 'A' ? 'hidden' : ''}>
+              가입승인
+            </option>
+            <option value="W" className={ustatus === 'W' ? 'hidden' : ''}>
+              서류반려
+            </option>
+            <option value="P" className={ustatus === 'P' ? 'hidden' : ''}>
+              대기
+            </option>
+          </select>
+        </div>
+      </div>
+    );
+  }
 };
 export default Status;
